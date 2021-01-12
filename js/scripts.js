9fdc837436b3dd2f0843f0726e042cba87b72e68
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-12 13:28:04 -0800

    Added classes and ID's to html, added some styling, added jQuery to toggle images and descriptions
    Co-authored-by: Louie Schoenknecht <luisesch97@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-
-
-
+  $("#gorilla").hide();
+  $("#lion").hide();
+  $("#sloth").hide();
 
   
+  $("#div1").click(function(){
+    $(".beginning").toggle();
+    $("#gorilla").toggle();
+  });
+
+  $("#div2").click(function(){
+    $(".beginning").toggle();
+    $("#sloth").toggle();
+  });
+
+  $("#div3").click(function(){
+    $(".beginning").toggle();
+    $("#lion").toggle();
+  });
+
+  $("#hiddenP1").click(function() {
+    $("#gorilla").toggle();
+    $(".beginning").toggle();
+  });
+
+  $("#hiddenP2").click(function() {
+    $("#sloth").toggle();
+    $(".beginning").toggle();
+  });
+
+  $("#hiddenP3").click(function() {
+    $("#lion").toggle();
+    $(".beginning").toggle();
+  });
+
+
 });
